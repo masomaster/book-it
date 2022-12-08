263a227516cf54ebc7947295634a92389c92a9dd
masomaster <masonlancaster@protonmail.com>
2022-12-08 14:55:49 -0800

    refines login page on mobile landscape

--- src/components/AuthPageBrand/AuthPageBrand.jsx
+++ src/components/AuthPageBrand/AuthPageBrand.jsx
@@ -1,6 +1,6 @@
 export default function AuthPageBrand() {
     return (
-        <div>
+        <div className="brand">
             <h1 className="book-it">BookIt!</h1>
             <h5>Helping people finish books since... well... yesterday.</h5>
         </div>
