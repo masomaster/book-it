4b9be2aa60383e75a74429ac0fd439eb0e02f90e
masomaster <masonlancaster@protonmail.com>
2022-12-09 11:45:02 -0800

    adds basic functioning hamburger menu on mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,15 +13,19 @@ export default function NavBar({user, setUser}) {
     }
     
     return (
-        <nav>
-            <div>
+        <nav className="top-nav">
+            <div id="title">
                 <Link to='/' className="book-it"><span className="book-it large">BookIt!</span></Link>
             </div>
-            <div className="other-links">
-                <Link to='/books' className="nav-links">Books</Link>
-                <Link to='/bookshelves' className="nav-links">Bookshelves</Link>
-                <button onClick={handleLogOut}>Log Out</button>
-            </div>
+            <input id="menu-toggle" type="checkbox" />
+            <label class='menu-button-container' for="menu-toggle">
+                <div class='menu-button'></div>
+            </label>
+            <ul className="menu">
+                <li><Link to='/books' className="nav-links">Books</Link></li>
+                <li><Link to='/bookshelves' className="nav-links">Bookshelves</Link></li>
+                <li><Link to="" onClick={handleLogOut}>Log Out</Link></li>
+            </ul>
         </nav>
     );
 }
